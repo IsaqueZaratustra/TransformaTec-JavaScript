16b492f905ff26792e23de9f08a370fc0202078c
IsaqueZaratustra <ADSisaque@gmail.com>
2022-04-29 12:24:25 -0300

    rev

--- Aula36/doWhile.js
+++ Aula36/doWhile.js
@@ -7,11 +7,3 @@ do{
 
 }while(i <= 10)
 //repete o loop enquanto a variável "i" for menor ou igual a 10, fazendo a verificação no final
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
